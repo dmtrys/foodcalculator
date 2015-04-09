53cd66f220b485c8422d489c92d6e509b784266b
Dmitry <dmtrys@inbox.lv>
2015-04-09 23:55:04 +0300

    temp functions

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
-function addRow(){
-	$( "#main_wrapper" ).append("<div class=\"product\"><div class=\"number\"></div>Product: <select><option></option></select> Weight: <input type=\"text\"> Gramms<input type=\"button\" value=\"Remove\" class=\"remove_row\"></div>");
+var fruits = [];
+fruits[0] = {name:"Banana", calories:"89"};
+fruits[1] = {name:"Apple", calories:"52"};
+fruits[2] = {name:"Potato", calories:"77"};
+fruits[3] = {name:"Tomato", calories:"18"};
+fruits[4] = {name:"Orange", calories:"47"};
 
+function addRow(){
+	$( "#main_wrapper" ).append("<div class=\"product\"><div class=\"number\"></div>Product: <select class=\"prod_list\"></select> Weight: <input type=\"text\" class=\"weight\"> Gramms<input type=\"button\" value=\"Remove\" class=\"remove_row\"></div>");
+	$('.prod_list').empty();
+	for (var i = 0; i < fruits.length; i++) {
+    $(".prod_list").append('<option value=\"'+i+'\">' + fruits[i].name + '</option>');
+}
+	
+	
 	$(".number").each(function( index ) {
 	$( this).empty();
 	$(this).append( index+1 );
@@ -25,7 +37,16 @@ function addRow(){
 	
 	});	
 }
-		
+
+function calculate(){
+		$(".prod_list").each(function() {
+			
+alert(fruits[$(this).val()].calories);
+alert($(this).val());
+	});	
+}
+
+
 $( document ).ready(function() {
 
 });	
