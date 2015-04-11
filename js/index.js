4e6f55af94f67af61d39f11b80b04c31f05dd33d
Dmitry <dmtrys@inbox.lv>
2015-04-11 10:16:08 +0300

    more calculation fields

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 var fruits = [];
-fruits[0] = {name:"Banana", calories:"89"};
-fruits[1] = {name:"Apple", calories:"52"};
-fruits[2] = {name:"Potato", calories:"77"};
-fruits[3] = {name:"Tomato", calories:"18"};
-fruits[4] = {name:"Orange", calories:"47"};
-fruits[5] = {name:"Apricot ", calories:"48"};
-fruits[6] = {name:"Avocado", calories:"160"};
-fruits[7] = {name:"Grapefruit", calories:"82"};
-fruits[8] = {name:"Grapes", calories:"114"};
-fruits[9] = {name:"Lemon ", calories:"17"};
-fruits[10] = {name:"Melon", calories:"24"};
+fruits[0] = {name:"Banana", calories:"89", carbs:"21.7", protein:"1.1", fat:"0.3"};
+fruits[1] = {name:"Apple", calories:"52", carbs:"13.8", protein:"0.3", fat:"0.2"};
+fruits[2] = {name:"Potato", calories:"110", carbs:"26", protein:"3", fat:"0"};
+fruits[3] = {name:"Tomato", calories:"27", carbs:"6", protein:"1", fat:"0"};
+fruits[4] = {name:"Orange", calories:"47", carbs:"21.5", protein:"1.7", fat:"0.2"};
+fruits[5] = {name:"Apricot ", calories:"48", carbs:"11.1", protein:"1.0", fat:"0.4"};
+fruits[6] = {name:"Avocado", calories:"160", carbs:"8.5", protein:"2.0", fat:"14.7"};
+fruits[7] = {name:"Grapefruit", calories:"82", carbs:"20.5", protein:"1.5", fat:"0.3"};
+fruits[8] = {name:"Grapes", calories:"114", carbs:"28.3", protein:"1.0", fat:"1.0"};
+fruits[9] = {name:"Lemon ", calories:"17", carbs:"5.4", protein:"0.6", fat:"0.2"};
+fruits[10] = {name:"Melon", calories:"24", carbs:"5.7", protein:"0.6", fat:"0.2"};
 
 function addRow(){
 	
@@ -51,29 +51,47 @@ function addRow(){
 
 function calculate(){
 	var fruitsArray = [];
+	var carbsArray = [];
+	var proteinArray = [];
+	var fatArray = [];
 
 	$(".product").each(function() {
 		a = fruits[$(this).find('.prod_list').val()].calories;
+		carbs = fruits[$(this).find('.prod_list').val()].carbs;
+		protein = fruits[$(this).find('.prod_list').val()].protein;
+		var fat = fruits[$(this).find('.prod_list').val()].fat;
 		b = $(this).find('.weight').val();
 		summ = ((a/100)*b);
-		fruitsArray.push(summ);                     
-	});	
+		carbsSumm = ((carbs/100)*b);
+		proteinSumm = ((protein/100)*b);
+		fatSumm = ((fat/100)*b);
+		fruitsArray.push(summ);
+		carbsArray.push(carbsSumm);
+		proteinArray.push(proteinSumm);
+		fatArray.push(fatSumm);
+	});		
 
 	var total = 0;
+	var totalCarbs = 0;
+	var totalProtein = 0;
+	var totalFat = 0;
 	for (var i = 0; i < fruitsArray.length; i++) {
-		total += fruitsArray[i] << 0;
+		total += fruitsArray[i];
+		totalCarbs += carbsArray[i];
+		totalProtein += proteinArray[i];
+		totalFat += fatArray[i];
 	}
 	
+	totalFat = Number((totalFat).toFixed(2));
+	totalProtein = Number((totalProtein).toFixed(2));
+	totalCarbs = Number((totalCarbs).toFixed(2));
+	total = Number((total).toFixed(2));
+
 	$( "#results_wrapper" ).find(".result").remove();
-	$( "#results_wrapper" ).append('<div class=\"result\"><span class=\"total\">Total calories:</span> '+total+'</div>');
+	$( "#results_wrapper" ).append('<div class=\"result\"><span class=\"total\">Total Calories:</span> '+total+' / <span class=\"total\">Carbs:</span> '+totalCarbs+' (g) / <span class=\"total\">Protein:</span> '+totalProtein+' (g) / <span class=\"total\">Fat:</span> '+totalFat+' (g)</div>');
 }
 
 function removeAll(){
 	$("#main_wrapper").empty();
 	$("#results_wrapper").empty();
 }
-
-$( document ).ready(function() {
-
-});
-
